a9a807bf60bf43167ed458258c9ea42882d30c4e
JoyZadan <joyzadan.learning@gmail.com>
2022-06-05 20:46:56 +0000

    Create init and models files, add models to models

--- paleorecipes/models.py
+++ paleorecipes/models.py
@@ -1,2 +1,34 @@
 """ imports """
 from paleorecipes import db
+
+
+class Category(db.Model):
+    # schema for the Category model
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(30), unique=True, nullable=False)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return self.category_name
+
+
+class Recipe(db.Model):
+    # schema for the Recipe model
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_name = db.Column(db.String(60), unique=True, nullable=False)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return self.recipe_name
+
+
+class User(db.Model):
+    # schema for the User model
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(40), unique=True, nullable=False)
+    password = db.Column(db.String(260), nullable=False)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return self.user_name
+

a01beed88245631916519997667f9e969c4f103d
JoyZadan <joyzadan.learning@gmail.com>
2022-06-08 17:24:08 +0000

    Fix: Remove sticky class on navbar to stop sidebar overlap on user profile page

---
+++ paleorecipes/static/js/script.js
@@ -0,0 +1,12 @@
+// navbar changes background colour on scroll
+// const navbar = document.querySelector('.navbar');
+
+// window.onscroll = function() {
+//   var top = window.scrollY;
+//   console.log(top);
+//   if (top >= 50) {
+//     navbar.classList.add('active');
+//   } else {
+//     navbar.classList.remove('active');
+//   }
+// }

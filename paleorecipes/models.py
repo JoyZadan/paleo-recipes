8ed1c935e8d3d25b60e17898ec231cfa6de0eb7f
JoyZadan <joyzadan.learning@gmail.com>
2022-06-10 08:55:24 +0000

    Feat: Add login functionality WIP

--- paleorecipes/models.py
+++ paleorecipes/models.py
@@ -3,6 +3,7 @@ from paleorecipes import db
 
 
 class Category(db.Model):
+    """ scheme for the Category model """
     # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(30), unique=True, nullable=False)
@@ -13,6 +14,7 @@ class Category(db.Model):
 
 
 class Recipe(db.Model):
+    """ scheme for the Recipe model """
     # schema for the Recipe model
     id = db.Column(db.Integer, primary_key=True)
     recipe_name = db.Column(db.String(60), unique=True, nullable=False)
@@ -23,6 +25,7 @@ class Recipe(db.Model):
 
 
 class Users(db.Model):
+    """ scheme for the Users model """
     # schema for the User model
     id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String(40), unique=True, nullable=False)
@@ -32,4 +35,3 @@ class Users(db.Model):
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
         return self.user_name
-
--- paleorecipes/routes.py
+++ paleorecipes/routes.py
@@ -29,7 +29,7 @@ def register():
 
     if request.method == "POST":
         # check if username already exists in db
-        existing_user = Users.query.filter(Users.user_name == \
+        existing_user = Users.query.filter(Users.user_name ==
                                           request.form.get
                                           ("username").lower()).all()
 
@@ -46,9 +46,11 @@ def register():
         db.session.add(user)
         db.session.commit()
 
+        new_user = db.session.add(user)
+
         # add user profile to mongodb
         user_profile = {
-            "user_id": (),
+            "user_id": str(new_user),
             "avatar_no": int(request.form.get("avatar_no")),
             "fave_recipes": [],
             "my_recipes": [],
@@ -65,14 +67,49 @@ def register():
     return render_template("register.html")
 
 
+# from Code Institute combined databases source code
+# amended for my requirements
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """
+        Check if user is already logged in, if so redirect user
+        to the profile page. Get user's username from the form,
+        check if it already exists in the database.
+        If it does, flash a message to the user and redirect
+        to the login page. Save user in postgres db,
+        insert user_profile in mongodb, put user
+        into session cookie and redirect to profile page.
+    """
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = Users.query.filter(Users.user_name ==
+                                           request.form.get("username").lower()).all()
 
+        if existing_user:
+            print(request.form.get("username"))
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user[0].password, request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                session["user"] = request.form.get("firstname")
+                flash("Welcome, {}".format(
+                    request.form.get("username")))
+                return redirect(url_for(
+                            "profile", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
 
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
 
-
-
-
+    return render_template("login.html")
 
 
 @app.route("/profile")
 def profile():
+    """ render the profile page """
     return render_template("profile.html")

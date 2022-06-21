bc50d3ca9f8fbe6d496700e522cec53d91941314
JoyZadan <joyzadan.learning@gmail.com>
2022-06-21 23:23:35 +0000

    Change configuration on init file by removing two lines replacing postgres with postgresql

--- paleorecipes/__init__.py
+++ paleorecipes/__init__.py
@@ -19,8 +19,6 @@ if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")  # local
 else:
     uri = os.environ.get("DATABASE_URL")
-    if uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgresql://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
 
 db = SQLAlchemy(app)

9a9aaa0519f7825c4a27693e2bc74a8075e8fed6
JoyZadan <joyzadan.learning@gmail.com>
2022-06-21 23:53:03 +0000

    Changes to env and init files

--- paleorecipes/__init__.py
+++ paleorecipes/__init__.py
@@ -14,11 +14,8 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 uri = os.environ.get("DATABASE_URL")
-
-if os.environ.get("DEVELOPMENT") == "True":
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")  # local
-else:
-    uri = os.environ.get("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
 
 db = SQLAlchemy(app)

94772bb74e01c1a7c8f7e80a86ca92679aa918da
JoyZadan <joyzadan.learning@gmail.com>
2022-08-02 13:54:30 +0000

    Add new Date function to copyright

--- paleorecipes/static/js/script.js
+++ paleorecipes/static/js/script.js
@@ -1,4 +1,4 @@
 // UPDATES COPYRIGHT IN FOOTER TO THE CURRENT YEAR FUNCTION
 // gets the element by ID and stores it in a constant
 const currentYear = document.querySelector("#current-year");
-currentYear.innerText = new getFullYear();
+currentYear.innerText = new Date().getFullYear();

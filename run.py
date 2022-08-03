457a6c747c287090206e2c82d4e62ed2c517f83e
JoyZadan <joyzadan.learning@gmail.com>
2022-08-03 16:04:51 +0000

    Set DEBUG=False

--- run.py
+++ run.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
-        debug=os.environ.get("DEBUG=True")
+        debug=os.environ.get("DEBUG=False")
     )

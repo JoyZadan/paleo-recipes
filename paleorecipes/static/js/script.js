e9e516a6cb0ea42b0f2840ace3f9f46721627225
JoyZadan <joyzadan.learning@gmail.com>
2022-08-03 17:48:32 +0000

    Edit comment on script.js

--- paleorecipes/static/js/script.js
+++ paleorecipes/static/js/script.js
@@ -1,4 +1,4 @@
-// UPDATES COPYRIGHT IN FOOTER TO THE CURRENT YEAR FUNCTION
+// UPDATES COPYRIGHT IN FOOTER TO THE CURRENT YEAR
 // gets the element by ID and stores it in a constant
 const currentYear = document.querySelector("#current-year");
 currentYear.innerText = new Date().getFullYear();
